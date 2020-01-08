6ca3d2cf488d8a241656ad9e895429efe19ea11e
Helen_Solovey <soloveyhelen0@gmail.com>
2020-01-08 13:10:51 +0300

    Add table with accounts

--- lwc/force-app/main/default/lwc/createContact/createContact.js
+++ lwc/force-app/main/default/lwc/createContact/createContact.js
@@ -1,11 +1,27 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 import { createRecord } from 'lightning/uiRecordApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import ACCOUNT_OBJECT from '@salesforce/schema/Account';
 import NAME_FIELD from '@salesforce/schema/Account.Name';
+import getOppdata from '@salesforce/apex/RecordCreateController.retriveAccounts';
+
+const columns = [
+  {
+    label: 'Account Name',
+    fieldName: 'AccountName',
+    type: 'text'
+  }, {
+    label: 'Account Owner',
+    fieldName: 'AccountOwner',
+    type: 'text'
+  }
+];
 
 export default class LdsCreateRecord extends LightningElement {
   @track accountId;
+  @track data = [];
+  @track columns = columns;
+
   name = '';
 
   handleNameChange(event) {
@@ -36,4 +52,24 @@ export default class LdsCreateRecord extends LightningElement {
         );
       });
   }
+  @wire(getOppdata)
+  opp({error, data}) {
+    if (data) {
+      let currentData = [];
+      data.forEach((row) => {
+        let rowData = {};
+        rowData.AccountName = row.Name;
+        rowData.AccountOwner = row.OwnerId;
+      //if (row.Account) {
+       // rowData.AccountName = row.Name;
+      //  rowData.AccountOwner = row.Owner.Name;
+       currentData.push(rowData);
+      });
+
+      this.data = currentData;
+    } else if (error) {
+      window.console.log(error);
+    }
+  }
+
 }
